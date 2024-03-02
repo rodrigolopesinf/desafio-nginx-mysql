160993c62b9f4851a2acb9ed8b5e47c47027b2c5
Rodrigo Lopes Silva <rodrigolopesinf@gmail.com>
2024-03-02 13:38:41 -0300

    Correções do docker-compose e index.js do node

--- node/index.js
+++ node/index.js
@@ -11,13 +11,39 @@ const config = {
 const mysql = require('mysql')
 const connection = mysql.createConnection(config)
 
-const sql = `INSERT INTO people(name) values('Rodrigo')`
+const sql = `DROP TABLE IF EXISTS people;`
 connection.query(sql)
-connection.end()
+
+const createTable = `CREATE TABLE people(id int not null auto_increment, name varchar(255), primary key(id));`
+connection.query(createTable)
+
+const names = ['Rodrigo', 'Lidiane', 'Julia']
+
+for (let i = 0; i < names.length; i++) {
+    connection.query('INSERT INTO people(name) values(?);', [names[i]])
+}
 
 app.get('/', (req, res) => {
-    res.send('<h1>Full Cycle Rocks!</h1>')
+    readData(res)
 })
+
+function readData(res) {
+    connection.query('SELECT * FROM people',
+        function (err, results, fields) {
+            if (err) throw err;
+            else console.log('Selected ' + results.length + ' row(s).');
+            var html = '<h1>Full Cycle Rocks!</h1><br/>'
+            for (i = 0; i < results.length; i++) {
+                html += results[i].name + '<br/>';
+            }
+            res.send(html);
+        })
+    connection.end(
+        function (err) {
+            if (err) throw err;
+            else console.log('Closing connection.')
+        });
+};
 
 app.listen(port, () => {
     console.log('rodando na porta' + port)
